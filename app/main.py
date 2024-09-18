7d9129dc0892b61b3561de429f993661ed96452d
roman <skyfoxwork@gmail.com>
2024-09-18 08:39:46 +0300

    serve_cars, calculate_washing_price

--- app/main.py
+++ app/main.py
@@ -20,3 +20,40 @@ class CarWashStation:
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
+
+    def serve_cars(self, cars: list) -> float:
+        result = 0
+        for car in cars:
+            if car.clean_mark < self.clean_power:
+                result += self.calculate_washing_price(car)
+                car.clean_mark = self.clean_power
+        return result
+
+    def calculate_washing_price(self, car):
+        cost = car.comfort_class * (self.clean_power - car.clean_mark) * \
+            self.average_rating / self.distance_from_city_center
+        # return round(cost)
+        return cost
+
+    def wash_single_car(self):
+        pass
+
+    def rate_service(self):
+        pass
+
+bmw = Car(comfort_class=3, clean_mark=3, brand='BMW')
+audi = Car(comfort_class=4, clean_mark=9, brand='Audi')
+
+print(bmw.clean_mark)  # 3
+
+wash_station = CarWashStation(
+    distance_from_city_center=5,
+    clean_power=6,
+    average_rating=3.5,
+    count_of_ratings=6
+)
+
+income = wash_station.serve_cars([bmw, audi])
+
+print(income)  # 6.3
+print(bmw.clean_mark)  # 6
