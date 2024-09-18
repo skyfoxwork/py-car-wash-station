787ae547eba4cc1d7787504ba03fdce7817b8af0
roman <skyfoxwork@gmail.com>
2024-09-18 11:47:21 +0300

    Solution-py-car-wash-station

--- app/main.py
+++ app/main.py
@@ -25,7 +25,7 @@ class CarWashStation:
         result = 0
         for car in cars:
             if car.clean_mark < self.clean_power:
-                result += self.calculate_washing_price(car)
+                result += self.wash_single_car(car)
                 car.clean_mark = self.clean_power
         return result
 
@@ -43,51 +43,5 @@ class CarWashStation:
         up_count_of_ratings = self.count_of_ratings + 1
         self.average_rating = round(
             (self.average_rating * self.count_of_ratings + grade)
-            / up_count_of_ratings)
+            / up_count_of_ratings, 1)
         self.count_of_ratings = up_count_of_ratings
-
-
-# # print("-"*30)
-#
-# bmw = Car(3, 3, 'BMW')
-# audi = Car(4, 9, 'Audi')
-# mercedes = Car(7, 1, 'Mercedes')
-#
-# ws = CarWashStation(6, 8, 3.9, 11)
-#
-# income = ws.serve_cars([
-#     bmw,
-#     audi,
-#     mercedes
-# ])
-#
-# # income == 41.7
-# print(income)
-#
-# # bmw.clean_mark == 8
-# # audi.clean_mark == 9
-# # mercedes.clean_mark == 8
-#
-# print(bmw.clean_mark)
-# print(audi.clean_mark)
-# print(mercedes.clean_mark)
-# # audi wasn't washed
-# # all other cars are washed to '8'
-#
-# ford = Car(2, 1, 'Ford')
-# wash_cost = ws.calculate_washing_price(ford)
-# # only calculating cost, not washing
-# # wash_cost == 9.1
-# # ford.clean_mark == 1
-#
-# print(wash_cost)
-# print(ford.clean_mark)
-#
-# print("-"*30)
-#
-# ws.rate_service(5)
-#
-# # ws.count_of_ratings == 12
-# # ws.average_rating == 4.0
-# print(ws.count_of_ratings)
-# print(ws.average_rating)
